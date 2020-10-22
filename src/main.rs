37f7bad3f370a72a2c68dc9a534704d31f990648
Jake McKnight <jake.mcknight@gmail.com>
2020-10-22 13:45:50 -0700

    Fix structures to match actual API results (AW's API docs are wrong).

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,93 @@
+#![allow(non_snake_case)]
 use serde::Deserialize;
 use reqwest::Error;
 
 #[derive(Deserialize, Debug)]
-struct Devices {
+struct Devices(Vec<Device>);
+
+#[derive(Deserialize, Debug)]
+struct Device {
     macAddress: String,
-    info: Info,
-    lastData: LastData
+    lastData: LastData,
+    info: Info
 }
 
 #[derive(Deserialize, Debug)]
-#[serde(rename_all = "lowercase")]
-enum Info {
-    String
+struct Info {
+    name: String,
+    coords: Coords
+}
+
+#[derive(Deserialize, Debug)]
+struct Coords {
+    coords: LonLat,
+    address: String,
+    location: String,
+    elevation: f64,
+    geo: Geo
+}
+
+#[derive(Deserialize, Debug)]
+struct Geo {
+    r#type: String,
+    coordinates: (f64, f64)
+}
+
+#[derive(Deserialize, Debug)]
+struct LonLat {
+    lon: f64,
+    lat: f64
 }
 
 #[derive(Deserialize, Debug)]
 struct LastData {
-    dateutc: i32,
-    date: String,
-    winddir: u8,
+    dateutc: i64,
+    tempinf: f64,
+    humidityin: f64,
+    baromrelin: f64,
+    baromabsin: f64,
+    tempf: f64,
+    battout: f32,
+    humidity: f64,
+    winddir: i16,
     windspeedmph: f64,
     windgustmph: f64,
     maxdailygust: f64,
-    windgustdir: i16,
-    winddir_avg2m: i16,
-    windspeedmph_avg2m: f64,
-    winddir_avg10m: i16,
-    windspeed_avg10m: f64,
-    tempf: f64,
-    humidity: f64,
-    baromrelin: f64,
-    baromabsin: f64,
-    tempinf: f64,
-    humidityin: f64,
     hourlyrainin: f64,
+    eventrainin: f64,
     dailyrainin: f64,
+    weeklyrainin: f64,
     monthlyrainin: f64,
-    yearlyrainin: f64,
+    totalrainin: f64,
+    solarradiation: f64,
+    uv: i8,
+    temp1f: f64,
+    humidity1: f64,
+    temp2f: f64,
+    humidity2: f64,
+    batt1: f32,
+    batt2: f32,
     feelsLike: f64,
-    dewPoint: f64
+    dewPoint: f64,
+    feelsLike1: f64,
+    dewPoint1: f64,
+    feelsLike2: f64,
+    dewPoint2: f64,
+    feelsLikein: f64,
+    dewPointin: f64,
+    lastRain: String,
+    tz: String,
+    date: String
 }
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-  let application_key = "";
-  let api_key = "";
-  let request_url = format!("https://api.ambientweather.net/v1/applicationKey={application_key}&apiKey={api_key}",
-                            application_key = application_key,
-                            api_key = api_key);
-  let response = reqwest::get(&request_url).await?;
-  let devices: Vec<Devices> = response.json().await?;
-  println!("{:?}", devices);
-  Ok(())
+async fn main() -> Result<(), Error> {
+    let application_key = "";
+    let api_key = "";
+    let request_url = format!("https://api.ambientweather.net/v1/devices?applicationKey={application_key}&apiKey={api_key}",
+                              application_key = application_key,
+                              api_key = api_key);
+    let response = reqwest::get(&request_url).await?.json::<Devices>().await?;
+    println!("{:#?}", response);
+    Ok(())
 }

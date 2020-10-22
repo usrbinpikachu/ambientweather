24e9826c8dba3f656c13966316d6df95cb1baa95
Jake McKnight <jake.mcknight@gmail.com>
2020-10-22 09:25:45 -0700

    Initial API interactions for validating structs Also cleaned up Cargo.toml and changed out restson for reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
-extern crate restson;
-extern crate chrono;
+use serde::Deserialize;
+use reqwest::Error;
 
-#[macro_use]
-extern crate serde_derive;
-
-use restson::{Error, RestClient, RestPath};
-use std::time::Duration;
-
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Devices {
     macAddress: String,
     info: Info,
     lastData: LastData
-};
+}
 
-struct Info {
-    name: String,
-    location: String
-};
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+enum Info {
+    String
+}
 
+#[derive(Deserialize, Debug)]
 struct LastData {
-    dateutc: DateTime,
+    dateutc: i32,
     date: String,
     winddir: u8,
     windspeedmph: f64,
@@ -43,4 +39,17 @@ struct LastData {
     yearlyrainin: f64,
     feelsLike: f64,
     dewPoint: f64
-};
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+  let application_key = "";
+  let api_key = "";
+  let request_url = format!("https://api.ambientweather.net/v1/applicationKey={application_key}&apiKey={api_key}",
+                            application_key = application_key,
+                            api_key = api_key);
+  let response = reqwest::get(&request_url).await?;
+  let devices: Vec<Devices> = response.json().await?;
+  println!("{:?}", devices);
+  Ok(())
+}

2ddb7e9d2f1f76c32ace192bf672c27e9ae87267
Jake McKnight <jake.mcknight@gmail.com>
2020-10-23 11:16:02 -0700

    Serde flatten allows us to avoid defining a massive struct full of optional fields

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
+use serde_json::Value;
 use reqwest::Error;
 
 pub type Devices = Vec<Device>;
@@ -42,158 +44,9 @@ pub struct Geo {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LastData {
-    dateutc: Option<i64>,
-    tempinf: Option<f64>,
-    humidityin: Option<i64>,
-    baromrelin: Option<f64>,
-    baromabsin: Option<f64>,
-    tempf: Option<f64>,
-    battout: Option<i64>,
-    humidity: Option<i64>,
-    winddir: Option<i64>,
-    windspeedmph: Option<f64>,
-    windgustmph: Option<f64>,
-    maxdailygust: Option<f64>,
-    hourlyrainin: Option<f64>,
-    eventrainin: Option<f64>,
-    dailyrainin: Option<f64>,
-    weeklyrainin: Option<f64>,
-    monthlyrainin: Option<f64>,
-    totalrainin: Option<f64>,
-    solarradiation: Option<f64>,
-    uv: Option<i64>,
-    #[serde(rename = "temp1f")]
-    temp1_f: Option<f64>,
-    humidity1: Option<i64>,
-    #[serde(rename = "temp2f")]
-    temp2_f: Option<f64>,
-    humidity2: Option<i64>,
-    #[serde(rename = "temp3f")]
-    temp3_f: Option<f64>,
-    humidity3: Option<i64>,
-    #[serde(rename = "temp4f")]
-    temp4_f: Option<f64>,
-    humidity4: Option<i64>,
-    #[serde(rename = "temp5f")]
-    temp5_f: Option<f64>,
-    humidity5: Option<i64>,
-    #[serde(rename = "temp6f")]
-    temp6_f: Option<f64>,
-    humidity6: Option<i64>,
-    #[serde(rename = "temp7f")]
-    temp7_f: Option<f64>,
-    humidity7: Option<i64>,
-    #[serde(rename = "temp8f")]
-    temp8_f: Option<f64>,
-    humidity8: Option<i64>,
-    #[serde(rename = "temp9f")]
-    temp9_f: Option<f64>,
-    humidity9: Option<i64>,
-    #[serde(rename = "temp10f")]
-    temp10_f: Option<f64>,
-    humidity10: Option<i64>,
-    batt1: Option<i64>,
-    batt2: Option<i64>,
-    batt3: Option<i64>,
-    batt4: Option<i64>,
-    batt5: Option<i64>,
-    batt6: Option<i64>,
-    batt7: Option<i64>,
-    batt8: Option<i64>,
-    batt9: Option<i64>,
-    batt10: Option<i64>,
-    #[serde(rename = "feelsLike")]
-    feels_like: Option<f64>,
-    #[serde(rename = "dewPoint")]
-    dew_point: Option<f64>,
-    #[serde(rename = "feelsLike1")]
-    feels_like1: Option<f64>,
-    #[serde(rename = "dewPoint1")]
-    dew_point1: Option<f64>,
-    #[serde(rename = "feelsLike2")]
-    feels_like2: Option<f64>,
-    #[serde(rename = "dewPoint2")]
-    dew_point2: Option<f64>,
-    #[serde(rename = "feelsLike3")]
-    feels_like3: Option<f64>,
-    #[serde(rename = "dewPoint3")]
-    dew_point3: Option<f64>,
-    #[serde(rename = "feelsLike4")]
-    feels_like4: Option<f64>,
-    #[serde(rename = "dewPoint4")]
-    dew_point4: Option<f64>,
-    #[serde(rename = "feelsLike5")]
-    feels_like5: Option<f64>,
-    #[serde(rename = "dewPoint5")]
-    dew_point5: Option<f64>,
-    #[serde(rename = "feelsLike6")]
-    feels_like6: Option<f64>,
-    #[serde(rename = "dewPoint6")]
-    dew_point6: Option<f64>,
-    #[serde(rename = "feelsLike7")]
-    feels_like7: Option<f64>,
-    #[serde(rename = "dewPoint7")]
-    dew_point7: Option<f64>,
-    #[serde(rename = "feelsLike8")]
-    feels_like8: Option<f64>,
-    #[serde(rename = "dewPoint8")]
-    dew_point8: Option<f64>,
-    #[serde(rename = "feelsLike9")]
-    feels_like9: Option<f64>,
-    #[serde(rename = "dewPoint9")]
-    dew_point9: Option<f64>,
-    #[serde(rename = "feelsLike10")]
-    feels_like10: Option<f64>,
-    #[serde(rename = "dewPoint10")]
-    dew_point10: Option<f64>,
-    #[serde(rename = "feelsLikein")]
-    feels_likein: Option<f64>,
-    #[serde(rename = "dewPointin")]
-    dew_pointin: Option<f64>,
-    #[serde(rename = "soiltemp1f")]
-    soil_temp1_f: Option<f64>,
-    #[serde(rename = "soilhum1")]
-    soil_hum1: Option<f64>,
-    #[serde(rename = "soiltemp2f")]
-    soil_temp2_f: Option<f64>,
-    #[serde(rename = "soilhum2")]
-    soil_hum2: Option<f64>,
-    #[serde(rename = "soiltemp3f")]
-    soil_temp3_f: Option<f64>,
-    #[serde(rename = "soilhum3")]
-    soil_hum3: Option<f64>,
-    #[serde(rename = "soiltemp4f")]
-    soil_temp4_f: Option<f64>,
-    #[serde(rename = "soilhum4")]
-    soil_hum4: Option<f64>,
-    #[serde(rename = "soiltemp5f")]
-    soil_temp5_f: Option<f64>,
-    #[serde(rename = "soilhum5")]
-    soil_hum5: Option<f64>,
-    #[serde(rename = "soiltemp6f")]
-    soil_temp6_f: Option<f64>,
-    #[serde(rename = "soilhum6")]
-    soil_hum6: Option<f64>,
-    #[serde(rename = "soiltemp7f")]
-    soil_temp7_f: Option<f64>,
-    #[serde(rename = "soilhum7")]
-    soil_hum7: Option<f64>,
-    #[serde(rename = "soiltemp1f")]
-    soil_temp8_f: Option<f64>,
-    #[serde(rename = "soilhum8")]
-    soil_hum8: Option<f64>,
-    #[serde(rename = "soiltemp9f")]
-    soil_temp9_f: Option<f64>,
-    #[serde(rename = "soilhum1")]
-    soil_hum9: Option<f64>,
-    #[serde(rename = "soiltemp10f")]
-    soil_temp10_f: Option<f64>,
-    #[serde(rename = "soilhum10")]
-    soil_hum10: Option<f64>,
-    #[serde(rename = "lastRain")]
-    last_rain: Option<String>,
-    tz: Option<String>,
-    date: Option<String>,
+  #[serde(flatten)]
+  #[serde(rename = "lastData")]
+  last_data: HashMap<String, Value>,
 }
 
 #[tokio::main]
